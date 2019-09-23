5fa863becd868b3b46bed8f3ae918b94aa5e6f28
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-23 13:01:54 -0400

    Add project files.

---
+++ MyResourceWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleDispose
+{
+    class MyResourceWrapper : IDisposable
+    {
+        public void Dispose()
+        {
+            // Clean up unmanaged resources...
+            // Dispose other contained disposable objects...
+            // Just for a test.
+            Console.WriteLine("***** In Dispose! *****");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Dispose *****\n");
+            // Create a disposable object and call Dispose() to free any internal resources.
+            MyResourceWrapper rw = new MyResourceWrapper();
+            if (rw is IDisposable)
+                rw.Dispose();
+            DisposeFileStream();
+
+            Console.ReadLine();
+        }
+
+        static void DisposeFileStream()
+        {
+            FileStream fs = new FileStream("myFile.txt", FileMode.OpenOrCreate);
+
+            // These method calls do the same thing. Confusing, to say the least.
+            fs.Close();
+            fs.Dispose();
+        }
+    }
+}
